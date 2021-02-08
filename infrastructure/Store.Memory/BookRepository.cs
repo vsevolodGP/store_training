77e4633765319e29910859fd8ecf01448df02345
vsevolodPG <vsevolodpalaguta@gmail.com>
2021-02-08 22:33:31 +0200

    Create Web application

--- infrastructure/Store.Memory/BookRepository.cs
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -1,8 +1,20 @@
 ﻿using System;
+using System.Linq;
 
 namespace Store.Memory
 {
-    public class Class1
+    public class BookRepository : IBookRepository
     {
+        private readonly Book[] books = new[]
+        {
+            new Book(1, "Art of programming"),
+            new Book(2, "Clean code"),
+            new Book(3, "OOP principles")
+        };
+
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            return books.Where(book => book.Title.Contains(titlePart)).ToArray();
+        }
     }
 }
