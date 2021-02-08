c4353a73b7a6721d3c920b305a8eb91cc4b8ea07
vsevolodPG <vsevolodpalaguta@gmail.com>
2021-02-08 09:45:52 +0200

    Create Book entity

---
+++ domain/Store/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Store
+{
+    public class Book
+    {
+        public int Id { get; }
+        public string Title { get; }
+
+        public Book(int id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+    }
+}
