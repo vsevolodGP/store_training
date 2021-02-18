8231f59955f636949dc684223446d3893a2b554f
vsevolodPG <vsevolodpalaguta@gmail.com>
2021-02-18 08:19:58 +0200

    Add cash payment

--- domain/Store/Order.cs
+++ domain/Store/Order.cs
@@ -17,9 +17,16 @@ namespace Store
             get { return items; }
         }
 
+        public string CellPhone { get; set; }
+
+        public OrderDelivery Delivery { get; set; }
+
+        public OrderPayment Payment { get; set; }
+
         public int TotalCount => items.Sum(item => item.Count);
 
-        public decimal TotalPrice => items.Sum(item => item.Price * item.Count);
+        public decimal TotalPrice => items.Sum(item => item.Price * item.Count)
+                                   + (Delivery?.Amount ?? 0m);
        
         public Order(int id, IEnumerable<OrderItem> items)
         {
---
+++ domain/Store/OrderPayment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store
+{
+    public class OrderPayment
+    {
+        public string UniqueCode { get; }
+
+        public string Description { get; }
+
+        public IReadOnlyDictionary<string, string> Parameters { get; }
+
+        public OrderPayment(string uniqueCode,
+                              string description,
+                              IReadOnlyDictionary<string, string> parameters)
+        {
+            if (string.IsNullOrWhiteSpace(uniqueCode))
+                throw new ArgumentException(nameof(uniqueCode));
+
+            if (string.IsNullOrWhiteSpace(description))
+                throw new ArgumentException(nameof(uniqueCode));
+
+            if (parameters == null)
+                throw new ArgumentNullException(nameof(parameters));
+
+            UniqueCode = uniqueCode;
+            Description = description;
+            Parameters = parameters;
+        }
+    }
+}
